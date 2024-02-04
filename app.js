d28113fab01ca370136f0bb6df180b4bc455c962
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-04 03:57:17 +0100

    module export and require

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ function Greetings(name) {
     console.log(`Hi ${name} good day , how are you doing`)
 }
 Greetings('Chidebere')
+var log = require('./logger')
+console.log(log)
---
+++ logger.js
@@ -0,0 +1,8 @@
+let url = "http://user/data/login.com";
+
+function Response(message) {
+  // response from the server
+}
+
+// Export the Response function directly
+module.exports.result = Response;
