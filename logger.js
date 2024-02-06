bf0513c8559af8318b37d10b8204cc37939abcc0
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-06 01:14:37 +0100

    created a node project

--- logger.js
+++ logger.js
@@ -1,11 +1,1 @@
-console.log(__filename);
-console.log(__dirname);
-let url = "http/user/data/login.com";
-
-function Response(message) {
-    console.log(message)
-  // response from the server
-}
-
-// Export the Response function directly
-module.exports = Response;
+const amount = 10
