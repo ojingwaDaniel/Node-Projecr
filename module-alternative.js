deaec06943dccd4e44e91d358188d61759fd3f0d
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-11 10:53:18 +0100

    node modules

--- module-alternative.js
+++ module-alternative.js
@@ -2,4 +2,11 @@ module.exports.shares = ['item1', 'item2']
 const guests = {
     name : 'bob'
 }
+//// another mind-blowing method 
+ const num1 = 20
+ const num2 = 25
+function Add() {
+    console.log( `The sum of the two numbers is ${num1 + num2}`)
+}
+Add()
 module.exports.chairman = guests
