99dc7ec74190b6e8fe556b0b2fe3b279c3b8b1bb
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-14 22:45:55 +0100

    internal modules of node js

--- internal-modules.js
+++ internal-modules.js
@@ -1,22 +1,22 @@
-const os = require('os')
-// info about the user 
-const user = os.userInfo()
-console.log(user)
+// const os = require('os')
+// // info about the user 
+// const user = os.userInfo()
+// console.log(user)
 
-// methods returns the system uptime
-const Uptime = os.uptime()
-console.log(`The system Uptime is ${Uptime} Seconds`)
-// more info
+// // methods returns the system uptime
+// const Uptime = os.uptime()
+// console.log(`The system Uptime is ${Uptime} Seconds`)
+// // more info
 
-const currentInfo = {
-    name: os.type(),
-    release: os.release(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem(),
+// const currentInfo = {
+//     name: os.type(),
+//     release: os.release(),
+//     totalMem: os.totalmem(),
+//     freeMem: os.freemem(),
     
    
-} 
-console.log(currentInfo);
+// } 
+// console.log(currentInfo);
 
 
 // paths
