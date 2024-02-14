cc55d83e31a78eeac58504684da5a29fd2b5d433
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-14 23:17:11 +0100

    internal modules of node js

--- internal-modules.js
+++ internal-modules.js
@@ -38,6 +38,9 @@ console.log(absolute)
 
 const { readFileSync, writeFileSync } = require('fs')
 const first = readFileSync('./content/first.txt','utf8')
-const second = readFileSync("./content/first.txt", "utf8");
+const second = readFileSync("./content/second.txt", "utf8");
 console.log(first);
 console.log(second);
+
+// writeSync
+writeFileSync('./content/result-sync.txt',`hello here is the result of your sync : ${first} and ${second}`)
