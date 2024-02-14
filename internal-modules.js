522c2ad8a39ac9eba5c049c227aadc6c08f2b509
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-14 22:50:38 +0100

    internal modules of node js

--- internal-modules.js
+++ internal-modules.js
@@ -1,24 +1,26 @@
-// const os = require('os')
-// // info about the user 
-// const user = os.userInfo()
-// console.log(user)
+const os = require('os')
+// info about the user 
+const user = os.userInfo()
+console.log(user)
 
-// // methods returns the system uptime
-// const Uptime = os.uptime()
-// console.log(`The system Uptime is ${Uptime} Seconds`)
-// // more info
+// methods returns the system uptime
+const Uptime = os.uptime()
+console.log(`The system Uptime is ${Uptime} Seconds`)
+// more info
 
-// const currentInfo = {
-//     name: os.type(),
-//     release: os.release(),
-//     totalMem: os.totalmem(),
-//     freeMem: os.freemem(),
+const currentInfo = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
     
    
-// } 
-// console.log(currentInfo);
+} 
+console.log(currentInfo);
 
 
 // paths
 const path = require('path')
 console.log(path.sep)
+// file-path
+const filePath = path.join('/content','subfolder','text.txt')
