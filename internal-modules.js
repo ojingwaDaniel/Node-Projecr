8a490f108a27a20658e65f10dc36fb8804bca3c4
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-15 00:02:01 +0100

    internal modules of node js

--- internal-modules.js
+++ internal-modules.js
@@ -1,48 +1,48 @@
-// const { log } = require('console')
-// const os = require('os')
-// // info about the user 
-// const user = os.userInfo()
-// console.log(user)
+const { log } = require('console')
+const os = require('os')
+// info about the user 
+const user = os.userInfo()
+console.log(user)
 
-// // methods returns the system uptime
-// const Uptime = os.uptime()
-// console.log(`The system Uptime is ${Uptime} Seconds`)
-// // more info
+// methods returns the system uptime
+const Uptime = os.uptime()
+console.log(`The system Uptime is ${Uptime} Seconds`)
+// more info
 
-// const currentInfo = {
-//     name: os.type(),
-//     release: os.release(),
-//     totalMem: os.totalmem(),
-//     freeMem: os.freemem(),
+const currentInfo = {
+    name: os.type(),
+    release: os.release(),
+    totalMem: os.totalmem(),
+    freeMem: os.freemem(),
     
    
-// } 
-// console.log(currentInfo);
+} 
+console.log(currentInfo);
 
 
-// // paths
-// const path = require('path')
-// console.log(path.sep)
-// // file-path
-// const filePath = path.join('/content', 'subfolder', 'text.txt')
-// console.log(filePath)
-// const basePath = path.basename(filePath)
-// console.log(basePath);
+// paths
+const path = require('path')
+console.log(path.sep)
+// file-path
+const filePath = path.join('/content', 'subfolder', 'text.txt')
+console.log(filePath)
+const basePath = path.basename(filePath)
+console.log(basePath);
 
-// // absolute path
-// const absolute = path.resolve(__dirname, 'content', 'subfolder', 'text.js')
-// console.log(absolute)
+// absolute path
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'text.js')
+console.log(absolute)
 
 
-// // file system
+// file system
 
-// // sync method
+// sync method
 
-// const { readFileSync, writeFileSync } = require('fs')
-// const first = readFileSync('./content/first.txt','utf8')
-// const second = readFileSync("./content/second.txt", "utf8");
-// console.log(first);
-// console.log(second);
+const { readFileSync, writeFileSync } = require('fs')
+const first = readFileSync('./content/first.txt','utf8')
+const second = readFileSync("./content/second.txt", "utf8");
+console.log(first);
+console.log(second);
 
 // writeSync
 writeFileSync('./content/result-sync.txt', `hello here is the result of your sync : ${first} and ${second}`, { flag: 'a' })
