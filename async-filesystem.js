0b35dcb836059c95393b7cb2cd8e0ed94877686e
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-16 01:29:00 +0100

    internal modules of node js

--- async-filesystem.js
+++ async-filesystem.js
@@ -1,29 +1,29 @@
-const { readFile, writeFile } = require("fs");
-console.log('start')
-readFile("./content/first.txt", "utf8", (err, result) => {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  const first = result;
-  readFile("./content/second.txt", "utf8", (err, result) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const second = result;
-    writeFile(
-      "./content/2nd-result-async.txt",
-      `Hello guys this the ${first} and the ${second} file`,
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        }
-        console.log("DOne qith this task");
+// const { readFile, writeFile } = require("fs");
+// console.log('start')
+// readFile("./content/first.txt", "utf8", (err, result) => {
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   const first = result;
+//   readFile("./content/second.txt", "utf8", (err, result) => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     const second = result;
+//     writeFile(
+//       "./content/2nd-result-async.txt",
+//       `Hello guys this the ${first} and the ${second} file`,
+//       (err, result) => {
+//         if (err) {
+//           console.log(err);
+//         }
+//         console.log("DOne qith this task");
         
-      }
-    );
-  });
-});
-console.log("starting a New one");
+//       }
+//     );
+//   });
+// });
+// console.log("starting a New one");
 
