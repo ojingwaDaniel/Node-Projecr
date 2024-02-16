58e1f83e46f12c9ce35bc3c8a00c7f5abfd82c52
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-16 03:52:25 +0100

    internal modules of node js

--- http-module.js
+++ http-module.js
@@ -1,7 +1,12 @@
-const http = require('http')
+/const http = require("http");
 const server = http.createServer((req, res) => {
-    res.write('Welcome to Our Tech Home')
-    res.end()
-    
-})
-server.listen(5000)
+    if (req.url == '/') {
+        res.end("Welcome to Danny's Tech Home")
+
+    } if (req.url == '/about') {
+        res.end('Here is a short History about our Company')
+    }
+    //  when the user enters acn address we dont have
+    res.end('<h1> Opps! </h1> <p> We seen not to find the page you are looking for </p> <a href = "/">please navigate back  tp the homePage </a> ')
+});
+server.listen(5000);
