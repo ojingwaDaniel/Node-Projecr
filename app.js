9abdd80731470d5cf293999194a0bb19a249f8fd
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-22 03:49:54 +0100

    updated app js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const _ = require('lodash')
 const nums = [1, [2, [3, [4]]]]
-const newNum = _.flatMapDeep(nums)
-console.log(newNum);
+const newNumArray = _.flatMapDeep(nums)
+console.log(newNumArray);
