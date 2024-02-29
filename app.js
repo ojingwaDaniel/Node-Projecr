777c0c68338ec0a89c3a8c2a43c49c17d8983f3c
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-29 06:36:18 +0100

    updated the app js

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const _ = require('lodash')
 const nums = [1, [2, [3, [4,[5]]]]]
 const newNumArray = _.flatMapDeep(nums)
 console.log(newNumArray);
-console.log('Hi guys this is node lesson practising to be good at it, wish me well.' )
+console.log('Hi guys this is node lesson practising to be good at it, wish me ' )
