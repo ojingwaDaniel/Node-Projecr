eefa5105bf91e7302c61c580b51c077e49b648f2
ojingwaDaniel <ojingwadanny@gmail.com>
2024-02-29 16:24:17 +0100

    updated the app js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ setTimeout(() => {
     console.log('Second')
 }, 0)
 console.log('third')
+///  Set interval opreating system process
+console.log('hiii im the first tas')
+setInterval(() => {
+     console.log('HIII im on an interval for 2 seconds')
+ },2000)
