3a514c92ff4a639852f26d47396ae8e7797f2557
ojingwaDaniel <ojingwadanny@gmail.com>
2024-03-01 21:25:49 +0100

    updated the app js

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ const server = http.createServer((req, res) => {
     console.log('resquest Event')
     res.end('Hi guys is me Again')
 })
+server.listen(5000, () => {
+    console.log('Listening at portal 5000 ....')
+})
