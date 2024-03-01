4ce09e7fb7c4736abe1e1c23a115b1132db308d4
ojingwaDaniel <ojingwadanny@gmail.com>
2024-03-01 20:40:07 +0100

    updated the app js

--- app.js
+++ app.js
@@ -6,5 +6,5 @@ console.log('third')
 ///  Set interval opreating system process
 console.log('hiii im the first tas')
 setInterval(() => {
-     console.log('HI im on an interval for 2 seconds')
+     console.log('HI im on an interval for 2 seconds enjoy the view')
  },2000)
