2d5cf9408148867cbb5306a19c495e55f8b44740
ojingwaDaniel <ojingwadanny@gmail.com>
2024-03-01 21:42:53 +0100

    updated the app js

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+// const http = require('http')
+// const server = http.createServer((req, res) => {
+//     console.log('resquest Event')
+//     res.end('Hi guys is me Again')
+// })
+// server.listen(5000, () => {
+//     console.log('Listening at portal 5000 ....')
+// })
 const http = require('http')
 const server = http.createServer((req, res) => {
-    console.log('resquest Event')
-    res.end('Hi guys is me Again')
+    
 })
 server.listen(5000, () => {
-    console.log('Listening at portal 5000 ....')
+    console.log('Server running at port : 5000 ....')
 })
