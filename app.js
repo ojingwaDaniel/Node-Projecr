b17f4b5651ff27e9ab91b3c42dc80ad4a9003204
ojingwaDaniel <ojingwadanny@gmail.com>
2024-03-03 00:56:35 +0100

    updated the app js

--- app.js
+++ app.js
@@ -8,6 +8,13 @@
 // })
 const http = require('http')
 const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.end('Home Page')
+    }
+    if (req.url === "/about") {
+      res.end("About Page");
+    }
+    res.end('Error Page')
     
 })
 server.listen(5000, () => {
